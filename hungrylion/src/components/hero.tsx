b4fcfbd27471781c814b2b3b24ab12a81651a47b
Dedios <dediosprogramming@gmail.com>
2023-07-05 00:14:35 -0400

    added spacing between hero & navbar and centered subheading

--- hungrylion/src/components/hero.tsx
+++ hungrylion/src/components/hero.tsx
@@ -3,39 +3,39 @@ import { SearchForm } from "./search.tsx";
 import { useState } from "preact/hooks";
 
 interface RecipeDetailsProps {
-    recipe: {
-        title: string;
-        serving: string;
-        ingredients: string;
-        directions: string;
-    };
+  recipe: {
+    title: string;
+    serving: string;
+    ingredients: string;
+    directions: string;
+  };
 }
 
 export default function Hero() {
-    const [foodData, setFoodData] = useState({});
+  const [foodData, setFoodData] = useState({});
 
-    const modifyFoodData = (data: RecipeDetailsProps) => {
-        setFoodData(data);
-    };
+  const modifyFoodData = (data: RecipeDetailsProps) => {
+    setFoodData(data);
+  };
 
-    return (
-        <div className="container flex items-center justify-center w-full mx-auto">
-            <div className="flex flex-col gap-4">
-                <div>
-                    <h1 className="text-6xl font-bold">Hungry? We got you.</h1>
-                </div>
-                <div>
-                    <h3 className="text-3xl">
-                        Find your next meal with Hungry Lion.
-                    </h3>
-                </div>
-                <div>
-                    <SearchForm setFoodData={modifyFoodData} />
-                </div>
-            </div>
-            <div>
-                <img src={Chef} />
-            </div>
+  return (
+    <div className="container flex items-center justify-center w-full mx-auto pt-9">
+      <div className="flex flex-col gap-4">
+        <div>
+          <h1 className="text-6xl font-bold">Hungry? We got you.</h1>
         </div>
-    );
+        <div>
+          <h3 className="text-3xl text-center">
+            Find your next meal with Hungry Lion.
+          </h3>
+        </div>
+        <div>
+          <SearchForm setFoodData={modifyFoodData} />
+        </div>
+      </div>
+      <div>
+        <img src={Chef} />
+      </div>
+    </div>
+  );
 } /* Hungry? We got you. */
