4aa79e500abc7b63870db825feb6a0c2ecb83259
Dedios <dediosprogramming@gmail.com>
2023-07-05 00:03:37 -0400

    positioning footer

--- hungrylion/src/components/footer.tsx
+++ hungrylion/src/components/footer.tsx
@@ -1,20 +1,23 @@
 const footer = () => {
- 
   return (
-    <div className="container flex justify-evenly w-full bg-gray-300">
-      <div className="box1 p-9">
-      <p class="font-bold">Made with love by Cerulian Lion</p>
-      <p class="text-center font-bold">&copy; 2023</p>
+    <div className="container flex justify-evenly min-w-full bg-gray-300 my-60 static">
+      <div className="box1 p-4">
+        <p>Copyright © 2023 w/ ❤️ by the Cerulean Lions</p>
       </div>
-      <div className="box2 p-3">
-      <h4 class="font-bold">Directory</h4>
-      <p className="underline"><a href="/recipes">Recipes</a></p>
-      <p className="underline"><a href="/meal-of-the-day">Meal of the Day</a></p>
-      <p className="underline"><a href="/cuisines">Cuisines</a></p>
-      </div>
+      {/* <div className="box2 p-3">
+        <h4 class="font-bold">Directory</h4>
+        <p className="underline">
+          <a href="/recipes">Recipes</a>
+        </p>
+        <p className="underline">
+          <a href="/meal-of-the-day">Meal of the Day</a>
+        </p>
+        <p className="underline">
+          <a href="/cuisines">Cuisines</a>
+        </p>
+      </div> */}
+    </div>
+  );
+};
 
-      </div>
-  )
-}
-
-export default footer
+export default footer;
